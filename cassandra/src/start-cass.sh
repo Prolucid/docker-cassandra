17b7cfe88b44983ae1031cf58fd4004d18aa11e7
Daniel Covello <daniel.covello@prolucid.ca>
2016-07-12 23:13:54 +0000

    Fixed cassandra not binding to docker ip address and added Authentication to cassandra

--- cassandra/src/start-cass.sh
+++ cassandra/src/start-cass.sh
@@ -4,4 +4,16 @@
 mkdir -p /var/lib/cassandra
 chown cassandra:cassandra /var/lib/cassandra
 
+CONFIG=/etc/cassandra
+
+IP=$(hostname --ip-address)
+sed -i -e "s,^listen_address.*,listen_address: $IP,g" $CONFIG/cassandra.yaml
+sed -i -e "s,^rpc_address.*,rpc_address: $IP,g" $CONFIG/cassandra.yaml
+sed -i -e "s,# broadcast_address.*,broadcast_address: $IP,g" $CONFIG/cassandra.yaml
+sed -i -e "s,# broadcast_rpc_address.*,broadcast_rpc_address: $IP,g" $CONFIG/cassandra.yaml
+sed -i -e "s,- seeds: \"127.0.0.1\",- seeds: \"$SEEDS\",g" $CONFIG/cassandra.yaml
+
+sed -i -e "s,authenticator: AllowAllAuthenticator,authenticator: PasswordAuthenticator,g" $CONFIG/cassandra.yaml
+sed -i -e "s,authorizer: AllowAllAuthorizer,authorizer: CassandraAuthorizer,g" $CONFIG/cassandra.yaml
+
 exec setuser cassandra cassandra -f 

99d2fbf4fd9c79eaae5f1ca8e0efb89a93e9a817
Daniel Covello <daniel.covello@prolucid.ca>
2017-07-20 18:26:38 +0000

    cass version 9-> 10 and enabled incremental backup config

--- cassandra/src/start-cass.sh
+++ cassandra/src/start-cass.sh
@@ -2,7 +2,9 @@
 
 # Setup data directory and permissions
 mkdir -p /var/lib/cassandra
-chown cassandra:cassandra /var/lib/cassandra
+chown -R cassandra:cassandra /var/lib/cassandra
+mkdir -p /var/log/cassandra
+chown -R cassandra:cassandra /var/log/cassandra
 
 CONFIG=/etc/cassandra
 
@@ -11,8 +13,8 @@ sed -i -e "s,^listen_address.*,listen_address: $IP,g" $CONFIG/cassandra.yaml
 sed -i -e "s,^rpc_address.*,rpc_address: $IP,g" $CONFIG/cassandra.yaml
 sed -i -e "s,# broadcast_address.*,broadcast_address: $IP,g" $CONFIG/cassandra.yaml
 sed -i -e "s,# broadcast_rpc_address.*,broadcast_rpc_address: $IP,g" $CONFIG/cassandra.yaml
-sed -i -e "s,- seeds: \"127.0.0.1\",- seeds: \"$SEEDS\",g" $CONFIG/cassandra.yaml
-
+sed -i -e "s|- seeds: \"127.0.0.1\"|- seeds: \"$SEEDS\"|g" $CONFIG/cassandra.yaml
+sed -i -e "s,incremental_backups:.*,incremental_backups: $ENABLE_INCREMENTAL_BACKUPS,g"       $CONFIG/cassandra.yaml
 sed -i -e "s,authenticator: AllowAllAuthenticator,authenticator: PasswordAuthenticator,g" $CONFIG/cassandra.yaml
 sed -i -e "s,authorizer: AllowAllAuthorizer,authorizer: CassandraAuthorizer,g" $CONFIG/cassandra.yaml
 
